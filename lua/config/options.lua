2eb6d97be9f5cd8a13325a4d91e12bdeadae731b
edcarneiro <99296433+Edyelgue@users.noreply.github.com>
2026-04-14 09:08:37 -0300

    Update line numbering and shell configuration
    Change relative line numbering to false and add PowerShell detection.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,17 @@
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.clipboard = "unnamedplus"
+
+-- Detecta PowerShell 7 (pwsh)
+if vim.fn.executable("pwsh") == 1 then
+  vim.opt.shell = "pwsh"
+  vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+else
+  vim.opt.shell = "cmd"
+  vim.opt.shellcmdflag = "/c"
+end
